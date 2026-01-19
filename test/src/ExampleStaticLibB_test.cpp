047521fd20666bcab2a1c047717a7c043d181da7
Mark <mark.rakhmatov@gmail.com>
2026-01-20 01:28:45 +0200

    feat: fix namespace in tests

--- test/src/ExampleStaticLibB_test.cpp
+++ test/src/ExampleStaticLibB_test.cpp
@@ -10,22 +10,22 @@ using namespace boost::ut;
 
 void tests() {
   "get string"_test = [] {
-    expect(!csl::getString().empty());
-    expect(csl::getString() == std::string("cpp static lib example"));
+    expect(!libB::getString().empty());
+    expect(libB::getString() == std::string("cpp static lib example"));
   };
 
   "factorial"_test = [] {
-    expect(csl::factorial(0) == 1_i);
-    expect(csl::factorial(1) == 1_i);
-    expect(csl::factorial(2) == 2_i);
-    expect(csl::factorial(12) == 479001600_i);
-    expect(csl::factorial(13) == -1_i);
+    expect(libB::factorial(0) == 1_i);
+    expect(libB::factorial(1) == 1_i);
+    expect(libB::factorial(2) == 2_i);
+    expect(libB::factorial(12) == 479001600_i);
+    expect(libB::factorial(13) == -1_i);
   };
 
   "factorial_constexpr"_test = [] {
-    static_assert(csl::factorialConstexpr(0) == 1_i);
-    static_assert(csl::factorialConstexpr(1) == 1_i);
-    static_assert(csl::factorialConstexpr(2) == 2_i);
+    static_assert(libB::factorialConstexpr(0) == 1_i);
+    static_assert(libB::factorialConstexpr(1) == 1_i);
+    static_assert(libB::factorialConstexpr(2) == 2_i);
   };
 }
 
